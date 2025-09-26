f83b9bbe061137f164eea4b88ecd72dd3fe79928
neekko33 <neekko33@gmail.com>
2025-09-26 11:28:48 +0800

    add vue3

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import { ref } from "vue";
+    const count = ref(0);
+    const increment = () => {
+        count.value++;
+    };
+</script>
+<template>
+    <h1>Hello, Vue!</h1>
+    <button @click="increment">Increment</button>
+    <p>Count: {{ count }}</p>
+</template>
