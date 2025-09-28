563177d66c6f9f878b211a74bad014fbd701f232
neekko33 <neekko33@gmail.com>
2025-09-28 14:44:14 +0800

    add breadcrumb

--- resources/js/components/layouts/Header.vue
+++ resources/js/components/layouts/Header.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { useRoute, RouterLink } from "vue-router";
+import { ref, watch } from "vue";
+
+const route = useRoute();
+const matchedRoutes = ref(route.matched);
+
+watch(route, (newValue) => {
+    matchedRoutes.value = newValue.matched;
+});
 </script>
 
 <template>
     <div class="pl-4 border-b border-gray-200 h-16 flex items-center">
         <div class="breadcrumbs text-sm">
             <ul>
-                <li><a>Home</a></li>
-                <li><a>Documents</a></li>
-                <li>Add Document</li>
+                <li v-for="(r, index) in matchedRoutes" :key="index">
+                    <RouterLink :to="r.path">{{ r.name }}</RouterLink>
+                </li>
             </ul>
         </div>
     </div>
